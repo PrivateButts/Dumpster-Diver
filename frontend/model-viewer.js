6a3fb961ccf772a92b553f3d4a1285eb7ea077fd
Randy Butts <rbutts@chq.org>
2021-09-16 03:37:17 -0400

    Added prototype of the model viewer

---
+++ frontend/model-viewer.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { createBackground } from './vignette/three-vignette.js';
+
+const container = document.querySelector("#model-preview")
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+container.appendChild( renderer.domElement );
+
+let IS_IOS = false;
+let vignette = createBackground({
+    aspect: camera.aspect,
+    grainScale: IS_IOS ? 0 : 0.001,
+    colors: [0xffffff, 0xaaaaaa]
+});
+vignette.name = 'Vignette';
+vignette.renderOrder = -1;
+scene.add(vignette);
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+
+loader.load( container.dataset.modelUrl, function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+camera.position.z = 5;
+camera.position.y = 3;
+
+let hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+hemiLight.position.set( 0, 300, 0 );
+scene.add( hemiLight );
+
+let dirLight = new THREE.DirectionalLight( 0xffffff );
+dirLight.position.set( 75, 300, -75 );
+scene.add( dirLight );
+
+function animate() {
+	requestAnimationFrame( animate );
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: {
         index: './frontend/index.js',
         snippet: './frontend/snippet-viewer.js',
+        model: './frontend/model-viewer.js',
     },
     output: {
         filename: '[name]-bundle.js', // output bundle file name
@@ -22,6 +23,14 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
+                ]
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
                 ]
             }
         ]
